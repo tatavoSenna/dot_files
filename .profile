0b8d8de64d43fdf0b79335721975033add7fdd5a
Luiz Senna <tatavo@MacBook-Pro-de-Luiz.local>
2014-11-29 16:56:52 -0200

    first draft

---
+++ .profile
@@ -0,0 +1,12 @@
+alias ga='git add .'
+alias gs='git status'
+alias gf='git fetch'
+alias gc='git commit -m'
+alias gck='git checkout'
+alias gckb='git checkout -b'
+alias gpull='git pull'
+alias gpush='git push'
+alias gdev='git checkout dev'
+alias runserver='python manage.py runserver'
+ 
+alias pdir='cd /Users/tatavo/Projetos'
---
+++ instala_tudo.sh
@@ -0,0 +1,9 @@
+#brew cask install android-studio;
+#brew cask install dropbox;
+#brew cask install google-chrome;
+#brew cask install evernote;
+#brew cask install sequel-pro;
+#curl -o get-pip.py https://bootstrap.pypa.io/get-pip.py;
+#curl -o sudogetpip.sh https://gist.github.com/tatavoSenna/c53a4e7fb715267e1734/download;
+#./sudogetpip.sh 
+sudo pip install virtualenv
